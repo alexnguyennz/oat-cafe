354388489a4eb396ade3613d4f663a0efa37c557
Alex <alexnguyennz7@gmail.com>
2026-07-07 23:43:05 +1000

    Update mobile menu nav styles

--- src/components/header/mobile-menu.tsx
+++ src/components/header/mobile-menu.tsx
@@ -44,14 +44,17 @@ export const MobileMenu = ({ pathname }: { pathname: string }) => {
               <a
                 href={link.href}
                 className={cn(
-                  "group underline-animation relative z-20 mx-2 inline-flex rounded-lg font-semibold text-white uppercase transition-colors before:h-0.75! hover:text-white",
+                  "relative isolate z-20 inline-flex overflow-hidden px-2 py-0.5 font-semibold uppercase transition-colors",
+                  "text-white before:absolute before:inset-x-0 before:bottom-0 before:z-0 before:h-0 before:bg-white before:content-['']",
+                  "before:transition-[height] before:duration-200 before:ease-out hover:text-black hover:before:h-full",
                   link.href === "/"
-                    ? pathname === "/" && "underline-active before:h-0.75!"
-                    : pathname.startsWith(link.href) && "underline-active",
+                    ? pathname === "/" && "bg-white text-black before:h-full"
+                    : pathname.startsWith(link.href) &&
+                        "bg-white text-black before:h-full",
                 )}
                 data-astro-prefetch
               >
-                {link.name}
+                <span className="relative z-10">{link.name}</span>
               </a>
             </li>
           ))}
